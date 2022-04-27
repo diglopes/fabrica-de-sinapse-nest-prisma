42322f400a76689b8a50022a2dce2776eaed09f2
Diego Lopes <diego-chez@hotmail.com>
2022-04-27 11:51:31 -0300

    feat(pokemon): add pokemon endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { MessagesController } from './messages/messages.controller';
 import { MessagesService } from './messages/messages.service';
 import { MessagesModule } from './messages/messages.module';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [MessagesModule, PokemonModule],
   controllers: [AppController, MessagesController],
   providers: [AppService, MessagesService],
 })
-export class AppModule {}
+export class AppModule { }
