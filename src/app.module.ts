a607ece55a81cc3c89a0d5b20ace6580ef9f3102
Diego Lopes <diego-chez@hotmail.com>
2022-04-27 09:07:26 -0300

    feat: add messages own module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessagesController } from './messages/messages.controller';
+import { MessagesService } from './messages/messages.service';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [],
+  imports: [MessagesModule],
   controllers: [AppController, MessagesController],
   providers: [AppService, MessagesService],
 })
