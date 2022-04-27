eb12bd384d892e7cf4e560183a50090683174eba
Diego Lopes <diego-chez@hotmail.com>
2022-04-27 13:41:56 -0300

    feat(pokemon): validate CreatePokemonDto

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,7 +1,11 @@
-import { Prisma } from "@prisma/client";
+import { IsInt, IsNotEmpty, IsString } from "class-validator";
+import { Pokemon } from "../entities/pokemon.entity";
 
-export class CreatePokemonDto implements Prisma.PokemonCreateInput {
+export class CreatePokemonDto extends Pokemon {
+    @IsString()
+    @IsNotEmpty()
     name: string;
+
+    @IsInt()
     height: number;
-    Images?: Prisma.ImageCreateNestedManyWithoutPokemonInput;
 }
